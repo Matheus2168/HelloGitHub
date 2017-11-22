b9b80a49c90e3f6e9a4569dabb251deb8e2897ae
Mateusz <Mateusz@192.168.0.94>
2017-11-22 22:27:59 +0100

    Dodanie kolejnych linii tekstu

--- zadanie1/src/zadanie1/Zadanie.java
+++ zadanie1/src/zadanie1/Zadanie.java
@@ -4,7 +4,7 @@ public class Zadanie {
 
 	public static void main(String[] args) {
 		System.out.println("Uczê");
-		System.out.println("sie \nprogramowaæ");
+		System.out.println("sie \nprogramowaæ \ni \ncommitowaæ");
 
 	}
 
